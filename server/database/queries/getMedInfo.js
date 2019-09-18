2ec91f48f547734550595a3cf6d2253b5facf88e
Muniralsharif <munir.sniper@gmail.com>
2019-09-18 11:15:33 +0300

    refactoring the queries

--- server/database/queries/getMedInfo.js
+++ server/database/queries/getMedInfo.js
@@ -1,13 +1,12 @@
 const dbConnection = require('./../db_connection')
 
 const getMedInfo = medicine => {
-  return dbConnection
-    .query(
-      'select pharmacy.pharmacyName, pharmacy.phone, pharmacy.location, pharmacy.workHours, pharmacy.offDays, medicine.name as medicineName, medicine.medicineID,medicine.img, medicine.description,medicine.prescription, medicine.company,pharmacy_to_medicine.soldOut, pharmacy_to_medicine.price from pharmacy inner join pharmacy_to_medicine on pharmacy_to_medicine.pharmacyID = pharmacy.pharmacyID inner join medicine on pharmacy_to_medicine.medicineID = medicine.medicineID where medicine.medicineID = (select medicineID from medicine where name ilike $1)',
-      [medicine]
-    )
-    .then(res => res.rows)
-    .catch(err => console.log(err))
+    return dbConnection
+        .query(
+            'select * from pharmacy inner join pharmacy_to_medicine on pharmacy_to_medicine.pharmacyID = pharmacy.pharmacyID inner join medicine on pharmacy_to_medicine.medicineID = medicine.medicineID where name ilike $1', [medicine]
+        )
+        .then(res => res.rows)
+        .catch(err => console.log(err))
 }
 
 module.exports = getMedInfo
